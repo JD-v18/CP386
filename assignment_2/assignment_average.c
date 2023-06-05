40e280cd4fd03cd5605f8e418fc48c870d17a985
JD <jidaandahiya112@gmail.com>
2023-06-05 17:38:27 +0000

    File rename and initial description setup

---
+++ assignment_2/assignment_average.c
@@ -0,0 +1,16 @@
+/*
+ -------------------------------------
+ Assignment 2
+ File: assignment_average.c
+ -------------------------------------
+ Author:  Jidaan Dahiya / Andrew Graco
+ ID:      211738270 / 210422740
+ Email:   dahi8270@mylaurier.ca /  grec2740@mylaurier.ca
+ Version  2023-06-05
+ -------------------------------------
+ */
+
+ //Andrew
+ #include <stdio.h>
+ #include <stdlib.h>
+
---
+++ assignment_2/process_management.c
@@ -0,0 +1,16 @@
+/*
+ -------------------------------------
+ Assignment 1
+ File: process_management.c
+ -------------------------------------
+ Author:  Jidaan Dahiya / Andrew Graco
+ ID:      211738270 / 210422740
+ Email:   dahi8270@mylaurier.ca / grec2740@mylaurier.ca
+ Version  2023-06-05
+ -------------------------------------
+ */
+
+// JD
+#include <stdio.h>
+#include <stdlib.h>
+
