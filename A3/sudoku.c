d8a58619a8dc24a2b6c047f4482c25e0544b8de9
JD <jidaandahiya112@gmail.com>
2023-06-25 22:36:50 -0400

    sudoku changes commit

--- A3/sudoku.c
+++ A3/sudoku.c
@@ -1,3 +1,17 @@
+/*
+ -------------------------------------
+ Assignment 3
+ File: sudoku.c
+ -------------------------------------
+ Author:  Jidaan Dahiya / Andrew Graco
+ ID:      211738270 / 210422740
+ Email:   dahi8270@mylaurier.ca / grec2740@mylaurier.ca
+ Version  2023-06-25
+ -------------------------------------
+ */
+
+// Andrew
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
